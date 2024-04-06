a85a47e8f79cca86a9dbe32a9a33e457f743b7a1
AmmarFK <farooqammar51@yahoo.com>
2024-04-07 07:26:01 +1000

    task 4.1P

--- index.js
+++ index.js
@@ -1,19 +1,137 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const winston = require("winston");
 
-const app = express();
-const PORT = process .env.PORT || 3000
-
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Define a route to serve your HTML page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "calculator-microservice" },
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
+  ],
 });
 
-// Start the server
+const app = express();
+
+app.set("view engine", "hbs");
+app.set("public_html", path.join(__dirname, "public_html"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var error;
+var result;
+var num1;
+var num2;
+
+app.get("/", (req, res) => {
+  num1 = 0;
+  num2 = 0;
+  res.render("home", { num1, num2 });
+});
+
+app.post("/add", (req, res) => {
+  num1 = parseFloat(req.body.num1);
+  num2 = parseFloat(req.body.num2);
+
+  logger.log({
+    level: "info",
+    message: `New add operation requested: ${num1} add ${num2}`,
+  });
+
+  if (isNaN(num1) || isNaN(num2)) {
+    num1 = 0;
+    num2 = 0;
+    error = "Invalid input. Please provide numeric values.";
+    return res.render("home", { error, num1, num2 });
+  }
+
+  result = num1 + num2;
+  res.render("home", { num1, num2, result });
+});
+
+app.post("/subtract", (req, res) => {
+  num1 = parseFloat(req.body.num1);
+  num2 = parseFloat(req.body.num2);
+
+  logger.log({
+    level: "info",
+    message: `New add operation requested: ${num1} subtract ${num2}`,
+  });
+
+  if (isNaN(num1) || isNaN(num2)) {
+    num1 = 0;
+    num2 = 0;
+    error = "Invalid input. Please provide numeric values.";
+    return res.render("home", { error, num1, num2 });
+  }
+
+  result = num1 - num2;
+  res.render("home", { num1, num2, result });
+});
+
+app.post("/multiply", (req, res) => {
+  num1 = parseFloat(req.body.num1);
+  num2 = parseFloat(req.body.num2);
+
+  logger.log({
+    level: "info",
+    message: `New add operation requested: ${num1} multiply ${num2}`,
+  });
+
+  if (isNaN(num1) || isNaN(num2)) {
+    num1 = 0;
+    num2 = 0;
+    error = "Invalid input. Please provide numeric values.";
+    return res.render("home", { error, num1, num2 });
+  }
+
+  result = num1 * num2;
+  res.render("home", { num1, num2, result });
+});
+
+app.post("/divide", (req, res) => {
+  num1 = parseFloat(req.body.num1);
+  num2 = parseFloat(req.body.num2);
+
+  logger.log({
+    level: "info",
+    message: `New add operation requested: ${num1} divide ${num2}`,
+  });
+
+  if (num2 == 0) {
+    num1 = 0;
+    num2 = 0;
+
+    error = "Cannot divide by 0";
+    return res.render("home", { error, num1, num2 });
+  } else if (isNaN(num1) || isNaN(num2)) {
+    num1 = 0;
+    num2 = 0;
+
+    error = "Invalid input. Please provide numeric values.";
+    return res.render("home", { error, num1, num2 });
+  }
+
+  result = num1 / num2;
+  res.render("home", { num1, num2, result });
+});
+
+app.post("/reset", (req, res) => {
+  error = "";
+  result = "";
+  num1 = 0;
+  num2 = 0;
+
+  res.render("home", { num1, num2, error, result });
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Web server running at: http://localhost:${PORT}`)
-    console.log("Type Ctrl+C to shut down the web server")
+  console.log(`Server is running on port ${PORT}`);
 });
